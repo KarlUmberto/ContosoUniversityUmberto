f86f7262bfe3e5923bb442ccae91fe6d77560b72
KarlUmberto <114060928+KarlUmberto@users.noreply.github.com>
2023-09-01 12:51:50 +0300

    Add classes, adde models

---
+++ ContosoUniversityUmberto/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace ContosoUniversityUmberto.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public int Lastname { get; set; }
+        public int Firstname { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
